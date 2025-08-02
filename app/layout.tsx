278c40fe2dd48cf96cc4270e2633c19ba2889051
Phạm Hoàng Tiến <tienph_tts@miichisoft.net>
2025-08-02 14:08:08 +0700

    fix: meta affiliate link

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,7 +28,8 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>  
-        <meta name="impact-site-verification" content="b94e4353-3cd6-40b7-a0ef-00378e410a02" />
+       <meta name="impact-site-verification" content="b94e4353-3cd6-40b7-a0ef-00378e410a02" />
+       <meta name="value" content="b94e4353-3cd6-40b7-a0ef-00378e410a02" />
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased relative`}
